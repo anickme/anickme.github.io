6973777bc64cb32bfe9378a38a6a2b79c254346b
anickme <kevin_alexander22@yahoo.co.id>
2025-06-29 21:05:26 +0700

    test nuxt deployment

--- nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  app: {
-    baseURL: '/'
-  },
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
